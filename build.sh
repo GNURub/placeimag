68510c5e1755b1d1595c5a06bf0986628823def8
Ruben Cid <rubencidlara@gmail.com>
2018-06-17 15:36:39 +0200

    gh pages

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-GOOS=windows GOARCH=386 go build -o bin/placeimag_386.exe placeimag.go
-GOOS=windows GOARCH=amd64 go build -o bin/placeimag.exe placeimag.go
-GOOS=darwin GOARCH=amd64 go build -o bin/placeimag_mac placeimag.go
-go build -o bin/placeimag placeimag.go
